6a8ea22263e45c917d050f42c3c2d25691a3e72a
tjololo <vemundg@gmail.com>
2016-09-28 21:51:25 +0200

    props movie-client

---
+++ movie-client/src/main/java/net/tjololo/poc/spring/controllers/MovieController.java
@@ -0,0 +1,7 @@
+package net.tjololo.poc.spring.controllers;
+
+/**
+ * Created by tjololo on 28.09.2016.
+ */
+public class MovieController {
+}
---
+++ movie-client/src/main/java/net/tjololo/poc/spring/transferobjects/Movie.java
@@ -0,0 +1,7 @@
+package net.tjololo.poc.spring.transferobjects;
+
+/**
+ * Created by tjololo on 28.09.2016.
+ */
+public class Movie {
+}
