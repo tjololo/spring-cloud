1bb3c3c1fb5f9d4a838ff7bd9b5b35ec31cc3b88
tjololo <vemundg@gmail.com>
2016-09-28 22:23:31 +0200

    Movie client

--- movie-client/src/main/java/net/tjololo/poc/spring/controllers/MovieController.java
+++ movie-client/src/main/java/net/tjololo/poc/spring/controllers/MovieController.java
@@ -1,7 +1,63 @@
 package net.tjololo.poc.spring.controllers;
 
+import net.tjololo.poc.spring.transferobjects.Movie;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.hateoas.Resources;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
+
 /**
  * Created by tjololo on 28.09.2016.
  */
+@RestController
+@RequestMapping("/movies")
 public class MovieController {
+    private RestTemplate restTemplate;
+    private String repositoryURI;
+
+    @Autowired
+    public MovieController(RestTemplate restTemplate, @Value("${repository.uri}") String repositoryURI) {
+        this.restTemplate = restTemplate;
+        this.repositoryURI = repositoryURI;
+    }
+
+    @RequestMapping(value = "", method = RequestMethod.GET)
+    public Collection<Movie> getMovies() {
+        return restTemplate
+                .exchange(
+                        repositoryURI + "/movies",
+                        HttpMethod.GET,
+                        null,
+                        new ParameterizedTypeReference<Resources<Movie>>() {
+                        }
+                )
+                .getBody()
+                .getContent();
+    }
+
+    @RequestMapping(value = "titles", method = RequestMethod.GET)
+    public Collection<String> getTitles() {
+        return restTemplate
+                .exchange(
+                        repositoryURI + "/movies",
+                        HttpMethod.GET,
+                        null,
+                        new ParameterizedTypeReference<Resources<Movie>>() {
+                        }
+                )
+                .getBody()
+                .getContent()
+                .stream()
+                .map(Movie::getTitle)
+                .collect(Collectors.toList());
+    }
 }
--- movie-client/src/main/java/net/tjololo/poc/spring/transferobjects/Movie.java
+++ movie-client/src/main/java/net/tjololo/poc/spring/transferobjects/Movie.java
@@ -4,4 +4,9 @@ package net.tjololo.poc.spring.transferobjects;
  * Created by tjololo on 28.09.2016.
  */
 public class Movie {
+    private String title;
+
+    public String getTitle() {
+        return title;
+    }
 }
