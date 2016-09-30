be196ad11fee3e36c0a777a41374b87503bf0ca9
tjololo <vemundg@gmail.com>
2016-09-30 18:16:11 +0200

    connected movie-registry to eureka-server

--- movie-repository/src/main/java/net/tjololo/poc/spring/MovieRepositoryApplication.java
+++ movie-repository/src/main/java/net/tjololo/poc/spring/MovieRepositoryApplication.java
@@ -7,12 +7,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.ConfigurableApplicationContext;
 
 import java.util.Arrays;
 import java.util.List;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class MovieRepositoryApplication {
 
     public static void main(String[] args) {
