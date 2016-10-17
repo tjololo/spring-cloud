7dfceeb93122ab9db64ea0ab5c999c5ebd2c78da
Gaukstad, Vemund <vemund.gaukstad@brreg.no>
2016-10-17 10:02:28 +0200

    added actor-client

---
+++ actor-client/src/main/java/net/tjololo/poc/spring/ActorClientApplication.java
@@ -0,0 +1,12 @@
+package net.tjololo.poc.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActorClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ActorClientApplication.class, args);
+	}
+}
---
+++ actor-client/src/test/java/net/tjololo/poc/spring/ActorClientApplicationTests.java
@@ -0,0 +1,16 @@
+package net.tjololo.poc.spring;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ActorClientApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
