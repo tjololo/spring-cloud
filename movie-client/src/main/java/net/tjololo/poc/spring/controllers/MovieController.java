998a5a8bf0f126df94f41286c80f69ccecd6a72c
tjololo <vemundg@gmail.com>
2016-10-20 20:26:23 +0200

    Added turbine config

--- movie-client/src/main/java/net/tjololo/poc/spring/controllers/MovieController.java
+++ movie-client/src/main/java/net/tjololo/poc/spring/controllers/MovieController.java
@@ -1,16 +1,13 @@
 package net.tjololo.poc.spring.controllers;
 
-import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import net.tjololo.poc.spring.services.MovieFeignService;
 import net.tjololo.poc.spring.transferobjects.Movie;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Collection;
-import java.util.Collections;
 import java.util.stream.Collectors;
 
 /**
